a6fb58cce39e2b68c5a18309b48f209da7b8753c
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-07 15:51:35 -0500

    coded along in class to create bookmarks app

---
+++ app.js
@@ -0,0 +1,20 @@
+// Dependencies
+const express = require("express");
+
+// Configuration
+const app = express();
+
+const bookmarksController = require("./controllers/bookmarksController.js");
+app.use("/bookmarks", bookmarksController);
+
+// Routes
+app.get("/", (req, res)=>{
+    res.send("Welcome to Bookmarks App")
+});
+
+app.get("*", (req, res)=>{
+    res.status(404).json({ error: "Page not found" });
+});
+
+// Export
+module.exports = app;
