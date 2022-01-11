a880f6cffec0d124939c48a98987b8529e321a3d
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-11 11:08:17 -0500

    able to get an individual bookmark, using its index.

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -2,8 +2,15 @@ const express = require("express");
 const bookmarkRoutes = express.Router();
 const bookmarksArr = require("../models/bookmark.js");
 
+//  This is: `/bookmarks`
 bookmarkRoutes.get("/", (req, res)=>{
     res.json(bookmarksArr);
+})
+//   This is: `/bookmarks/3`
+bookmarkRoutes.get("/:index", (req, res)=>{
+    const { index } = req.params;
+    console.log(index);
+    res.json(bookmarksArr[index]);
 })
 
 module.exports = bookmarkRoutes;
